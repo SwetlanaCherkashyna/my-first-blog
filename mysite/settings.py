0c4ca81540b016ecabb34c61febf5f60fbf5f515
Svitlana Cherkashyna <svetlana0620@gmail.com>
2018-01-20 14:06:59 +0100

    Second revision

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jbktnqcn(ofllsmog--kj6e$kj(i9ri5(*tb$)-@jqq5n09_v8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
